dc901cece3e1191b269126ec2ed24302896aad77
Puneet Singh <puneet.royal2@gmail.com>
2026-02-10 19:29:09 +0530

    updated sheet link

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function initLeadForm() {
 
             // Save to Google Sheet via Apps Script
             try {
-                const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/d/AKfycbzYyDylFy5_0ysfYXbEXrI073kfP0zUrGMeD6oFMtBkeJ_3ZR5FiFbJaTEw7aOLGJ4I/userweb?v=1';
+                const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyM0las2qlqwxSRBpv9cPlEvbRY_you2upLExOkwJyV6OMMfAepAwgbm-M_FJYPacH_bw/exec';
                 const formData = new FormData();
                 formData.append('Email', email);
                 formData.append('Type', 'Lead Capture');
